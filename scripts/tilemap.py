3c0373b0645bb5bf101ff411305e0b4791eacc11
Codebulous <codebulous@gmail.com>
2023-12-19 18:41:49 +0800

    render only visible tiles to the screen

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -3,6 +3,7 @@ import pygame
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
 PHYSICS_TILES = {'grass', 'stone'}
 
+
 class Tilemap:
     def __init__(self, game, tile_size=16):
         self.game = game
@@ -38,6 +39,9 @@ class Tilemap:
         for tile in self.offgrid_tiles:
             surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
 
-        for location in self.tilemap:
-            tile = self.tilemap[location]
-            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+        for x in range(offset[0] // self.tile_size, (offset[0] + surface.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surface.get_height()) // self.tile_size + 1):
+                location = str(x) + ';' + str(y)
+                if location in self.tilemap:
+                    tile = self.tilemap[location]
+                    surface.blit(self.game.assets[tile['type']][tile['variant']], (x * self.tile_size - offset[0], y * self.tile_size - offset[1]))
