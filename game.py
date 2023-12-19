296a633c1b398016e2cc6236ce3df0308b54c709
Codebulous <codebulous@gmail.com>
2023-12-19 17:36:10 +0800

    added tile map

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import sys
 import pygame
 from pygame.locals import *
 
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
+
 
 class Game:
     def __init__(self):
@@ -13,14 +16,42 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
+        }
+
+        self.tilemap = Tilemap(self)
+
     def run(self):
         while True:
             self.display.fill((14, 219, 248))
+
+            self.tilemap.render(self.display)
 
             for event in pygame.event.get():
                 if (event.type == QUIT) or (event.type == KEYUP and event.key == K_ESCAPE):
                     pygame.quit()
                     sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_a:
+                        self.movement[0] = True
+                    if event.key == K_d:
+                        self.movement[1] = True
+                if event.type == KEYUP:
+                    if event.key == K_a:
+                        self.movement[0] = False
+                    if event.key == K_d:
+                        self.movement[1] = False
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
 
---
+++ scripts/tilemap.py
@@ -0,0 +1,43 @@
+import pygame
+
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {'grass', 'stone'}
+
+class Tilemap:
+    def __init__(self, game, tile_size=16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+        for i in range(10):
+            self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_location = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+
+        for offset in NEIGHBOR_OFFSETS:
+            check_location = str(tile_location[0] + offset[0]) + ';' + str(tile_location[1] + offset[1])
+            if check_location in self.tilemap:
+                tiles.append(self.tilemap[check_location])
+
+        return tiles
+
+    def physics_rects_around(self, pos):
+        rects = []
+
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+
+        return rects
+
+    def render(self, surface):
+        for tile in self.offgrid_tiles:
+            surface.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+
+        for location in self.tilemap:
+            tile = self.tilemap[location]
+            surface.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
