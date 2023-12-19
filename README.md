38e223437a2b6cef663269fc85706dda5c5e3488
Codebulous <codebulous@gmail.com>
2023-12-19 17:27:31 +0800

    initialized game application and added data resources

---
+++ README.md
@@ -0,0 +1,17 @@
+# Shadow Strike
+
+![logo](https://github.com/josephchay/shadow-strike/assets/136827046/42a06ec6-d2f5-44ae-8f03-5e587014091d)
+
+---
+
+# About
+
+**Shadow Strike** is an exhilarating platformer game set in the heart of nature. 
+As a skilled ninja warrior, you'll traverse lush fields, dense forests, 
+and perilous terrains. Master parkour moves, wield deadly weapons, and outwit cunning enemies in your quest for glory. 
+Unveil a mysterious plot and restore balance to a world enshrouded in darkness. 
+With stunning visuals and heart-pounding action, **Shadow Strike** offers an immersive ninja experience, testing your agility and strategy. 
+
+_Can you conquer the wild and become a true ninja legend..._
+
+---
---
+++ game.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from pygame.locals import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Ninja Game')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            self.display.fill((14, 219, 248))
+
+            for event in pygame.event.get():
+                if (event.type == QUIT) or (event.type == KEYUP and event.key == K_ESCAPE):
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    Game().run()
